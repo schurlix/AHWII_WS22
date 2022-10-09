d7daaebbdebc5705eb33cdda9eba869370a77357
Georg Graf SPG <grafg@spengergasse.at>
2022-10-09 21:03:08 +0200

    wieder push auf main

--- GRAFG/TicketautomatGRAFG.cs
+++ GRAFG/TicketautomatGRAFG.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace automat;    
 class TicketautomatGRAFG
+{
+    private String Standort { get; set; }
+    private int ticketpreis;
+    public TicketautomatGRAFG(String Standort, int ticketPreis)
     {
-    public override string ToString() => "Ich bin ein Ticketautomat GRAFG";
+        this.Standort = Standort;
+        this.ticketpreis = ticketPreis;
+    }
+    public override string ToString()
+    {
+        return $"Ich bin ein Ticketautomat GRAFG, stehe in {this.Standort}, jedes Ticket kostet bei mir {ticketpreis} €.";
+    }
 }
 
--- Program.cs
+++ Program.cs
@@ -8,8 +8,7 @@ class Program
     static void Main(string[] args)
     {
         //Ticketautomat tick = new Ticketautomat("Westbahnhof", 3);
-        var tick = new TicketautomatGRAFG();
+        var tick = new TicketautomatGRAFG("Westbahnhof", 3);
         Console.WriteLine(tick);
-        Console.WriteLine("Hallo!");
     }
 }
