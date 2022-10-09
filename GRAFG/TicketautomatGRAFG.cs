1953f6b861c73b61c64bd1acdea6273e14171827
Georg Graf SPG <grafg@spengergasse.at>
2022-10-09 20:30:34 +0200

    Ticketautomat in meinem Unterverzeichnis

---
+++ GRAFG/TicketautomatGRAFG.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automat;    
+class TicketautomatGRAFG
+    {
+    public override string ToString() => "Ich bin ein Ticketautomat GRAFG";
+}
+
--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ class Program
     static void Main(string[] args)
     {
         //Ticketautomat tick = new Ticketautomat("Westbahnhof", 3);
-        Ticketautomat tick = new Ticketautomat();
+        var tick = new TicketautomatGRAFG();
         Console.WriteLine(tick);
+        Thread.Sleep(1000);
+        Console.WriteLine("Hallo!");
+        Thread.Sleep(1000);
     }
 }
