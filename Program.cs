54c499c02c2730af09d5b45726348559a6d2705d
Georg Graf SPG <grafg@spengergasse.at>
2022-10-09 20:01:26 +0200

    import der unterrichtsfiles

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+﻿using System;
+
+namespace automat;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Ticketautomat tick = new Ticketautomat("Westbahnhof", 3);
+        Ticketautomat tick = new Ticketautomat();
+        Console.WriteLine(tick);
+    }
+}
---
+++ Ticketautomat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automat;    
+class Ticketautomat
+    {
+        // public String 
+       // public override string ToString()
+       // {
+       //     return "Ich bin ein Ticketautomat am {"
+       // }
+    }
+
