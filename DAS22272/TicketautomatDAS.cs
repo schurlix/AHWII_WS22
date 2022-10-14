81a973f212b575b317c966f7767f67f6490c40c8
Ben Daschner <DAS22272@spengergasse.at>
2022-10-14 08:21:33 +0200

    Class update

---
+++ DAS22272/TicketautomatDAS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automat;    
+class TicketautomatDAS
+{
+    private String Standort { get; set; }
+    private int ticketpreis;
+    public TicketautomatDAS(String Standort, int ticketPreis)
+    {
+        this.Standort = Standort;
+        this.ticketpreis = ticketPreis;
+    }
+    public override string ToString()
+    {
+        return $"Ich bin ein Ticketautomat DAS22272, stehe in {this.Standort}, jedes Ticket kostet bei mir {ticketpreis} €.";
+    }
+}
+
