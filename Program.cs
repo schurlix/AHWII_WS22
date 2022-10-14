f3369041adbc984c919c3d43a2e8129aa1ab25f6
Ben Daschner <DAS22272@spengergasse.at>
2022-10-14 08:25:54 +0200

    Text

--- Program.cs
+++ Program.cs
@@ -11,5 +11,6 @@ class Program
         var tick = new TicketautomatGRAFG("Westbahnhof", 3);
         var tick1 = new TicketautomatDAS("Matzleinsdorfer Platz", 5);
         Console.WriteLine(tick);
+        Console.WriteLine(tick1);
     }
 }
