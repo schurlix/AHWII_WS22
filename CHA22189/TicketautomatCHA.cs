cb2b77eb6804a7ac6885e4da878682e97fe3bf07
Amin Challakhi <CHA22189@spengergasse.at>
2022-10-14 09:30:29 +0200

    Test

---
+++ CHA22189/TicketautomatCHA.cs
@@ -0,0 +1,38 @@
+﻿using automat;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace automat;    
+class TicketautomatCHA : Ticketautomat
+{
+    public String Standort { get; set; }
+    public int ticketpreis { get; set; }
+    public int eingeworfen { get; }
+
+    public int gesamtEinnahmen { get; }
+    public TicketautomatCHA(String Standort, int ticketPreis)
+    {
+        this.Standort = Standort;
+        this.ticketpreis = ticketPreis;
+        this.eingeworfen = 0;
+        this.gesamtEinnahmen = 0;
+    }
+    public override string ToString()
+    {
+        return $"Ich bin ein Ticketautomat GRAFG, stehe in {this.Standort}, jedes Ticket kostet bei mir {ticketpreis}€.";
+    }
+     
+    public void einwerfen(int betrag)
+    {    }
+
+    bool ticketDrucken()
+    {
+        return false;
+    }
+    int wechselGeldAuszahlen()
+    { return 0; }
+}
+
